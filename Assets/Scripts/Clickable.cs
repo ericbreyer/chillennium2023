caff4c524322051a9abd39ebf78b0098f8d8f776
Eric Breyer <52176894+ericbreyer@users.noreply.github.com>
2023-02-24 22:56:30 -0600

    cluckable

---
+++ Assets/Scripts/Clickable.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+
+
+    private IClickHandler callback;
+    public GameObject hoverIndicator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!TryGetComponent<IClickHandler>(out callback)) {
+            callback = new DummyClick();
+        }
+        hoverIndicator.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void OnMouseEnter() {
+        hoverIndicator.SetActive(true);
+        callback.DoHoverEnter();
+    }
+    private void OnMouseExit() {
+        hoverIndicator.SetActive(false);
+        callback.DoHoverLeave();
+    }
+    private void OnMouseOver() {
+        if(Input.GetMouseButtonDown(0)) {
+            callback.DoClick();
+        }
+    }
+}
+
+public interface IClickHandler {
+    public void DoClick();
+    public void DoHoverEnter();
+    public void DoHoverLeave();
+}
+
+public class DummyClick : IClickHandler {
+    public void DoClick() {
+    }
+
+    public void DoHoverEnter() {
+    }
+
+    public void DoHoverLeave() {
+    }
+}
