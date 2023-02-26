35a7df7ef0a73a461cb84e71a4ee872c16d9f00d
Eric Breyer <52176894+ericbreyer@users.noreply.github.com>
2023-02-25 20:31:21 -0600

    psh go

--- Assets/Scripts/Tiles and Towers/FarmTower.cs
+++ Assets/Scripts/Tiles and Towers/FarmTower.cs
@@ -6,17 +6,20 @@ using UnityEngine;
 public class FarmTower : Tower, IClickHandler
 {
     private ArtichokeManager bank;
+    private GameController controller;
     private SpriteRenderer sr;
-    [SerializeField] private int rate;
+    [SerializeField] private int myrate;
     [SerializeField] private Sprite growingSprite;
     [SerializeField] private Sprite grownSprite;
+    [SerializeField] private Sprite fireSprite;
     [SerializeField] private GameObject hi;
     public new int type = 1;
 
     private enum GrowStste {
         dead,
         growing,
-        grown
+        grown,
+            fire,
     }
     private GrowStste grown = GrowStste.dead;
 
@@ -25,6 +28,7 @@ public class FarmTower : Tower, IClickHandler
     {
         bank = GameObject.FindObjectOfType<ArtichokeManager>();
         sr = gameObject.GetComponent<SpriteRenderer>();
+        controller = FindObjectOfType<GameController>();
         hi.SetActive(false);
     }
 
@@ -32,11 +36,19 @@ public class FarmTower : Tower, IClickHandler
         if (grown == GrowStste.grown) {
             sr.sprite = grownSprite;
         }
-        else { sr.sprite = growingSprite;}
+        else if(grown == GrowStste.growing) { sr.sprite = growingSprite;}
 
         if (grown == GrowStste.dead) {
             grown = GrowStste.growing;
             Util.SetTimeout(() => { this.grown = GrowStste.grown; }, 7000, this);
+        }
+
+        var rand = Random.value;
+        if(rand * 100 < controller.getWarmth()) {
+            sr.sprite = fireSprite;
+            grown = GrowStste.fire;
+            Util.SetTimeout(() => { this.home.detach(); Destroy(gameObject); }, 2000, this);
+
         }
     }
 
