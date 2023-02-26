f3cb687856e04979b689aec5920e68850882d016
Eric Breyer <52176894+ericbreyer@users.noreply.github.com>
2023-02-25 20:11:32 -0600

    More towers and stuff idk its been a while

---
+++ Assets/Scripts/Tiles and Towers/CarbonCapture.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CarbonCapture : Tower
+{
+    private GameController gm;
+    [SerializeField] private float rate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.FindObjectOfType<GameController>();
+    }
+
+    private void FixedUpdate() {
+        if(Time.frameCount % rate == 0) {
+            gm.changeWarmth(-.01f);
+        }
+    }
+}
